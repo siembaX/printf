5bcac5a96fe8c9bd0f4ec5fd2471cfe0d931c4e3
Mariokings <ozorkingsley168@yahoo.com>
2022-08-09 19:19:37 -0700

    print_address.c done

--- print_address.c
+++ print_address.c
@@ -6,7 +6,7 @@
  * @stop: stopping address
  * @except: except address
  *
- * Return: number bytes printed
+ * Return: number of bytes printed
  */
 int print_from_to(char *start, char *stop, char *except)
 {
